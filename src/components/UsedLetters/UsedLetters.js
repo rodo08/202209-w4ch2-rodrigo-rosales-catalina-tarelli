24886e3b9c1d6feef396250d4a98238f7faa7974
Catalina Tarelli <catatarelli@hotmail.com>
2022-10-18 22:45:04 +0200

    Modify UsedLetters component and App component

--- src/components/UsedLetters/UsedLetters.js
+++ src/components/UsedLetters/UsedLetters.js
@@ -1,11 +1,12 @@
 import "./UsedLetters.css";
 
-const UsedLetters = ({ usedLetters }) => {
+const UsedLetters = () => {
+  const usedLettersList = ["a", "b", "c"];
   return (
     <section className="used-letters-container">
       <h2>Used letters</h2>
       <ul className="used-letters">
-        {usedLetters.map((letter, key) => (
+        {usedLettersList.map((letter, key) => (
           <li key={key} className="used-letter">
             {`${letter}, `}
           </li>
